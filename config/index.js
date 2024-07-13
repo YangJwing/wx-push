ea197a3074418b6b4a54888691a87dd805fa3527
YangJwing <51504066+YangJwing@users.noreply.github.com>
2024-07-14 01:07:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wx018a785b3d7b9065",
   appsecret: "b179eea323101208ca515b176f32917b",
   template_id: "rSP3Qgc1gE_o63g2AiMOmkQCENLdSkxkKoq1ETBRGbw",
-  touser: "ozMof69iSThPtAS0HsJcFCtfcVO0",
+  touser: ["ozMof69iSThPtAS0HsJcFCtfcVO0","ozMof65jnWTTnwhCsecsrqT3nk9c"],
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "东莞",
